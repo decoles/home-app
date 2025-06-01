bfdb8cbdf4685dd1aa8fdecdd8380f25200949f3
David Coles <davidepcoles@gmail.com>
2025-06-01 07:34:01 +0000

    need to fix db

--- Backend/Utilities/Databasecontext.cs
+++ Backend/Utilities/Databasecontext.cs
@@ -7,16 +7,5 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<TransactionRecord> Transactions { get; set; }
-
     public DbSet<Tag> Tags { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<TransactionRecord>()
-            .HasMany(t => t.Tags)
-            .WithMany(t => t.Transactions)
-            .UsingEntity(j => j.ToTable("TransactionTags"));
-    }
 }
